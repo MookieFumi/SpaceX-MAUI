9a18cb9b9f2b83dd8a748509b2701e204267c56e
Miguel Angel Martin Hernandez <mookiefumi@outlook.com>
2024-06-05 15:46:08 +0200

    Add RelayCommand attribute

--- SpaceXData/ViewModels/LaunchesViewModel.cs
+++ SpaceXData/ViewModels/LaunchesViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SpaceXData.Services;
@@ -22,15 +21,11 @@ public partial class LaunchesViewModel : BaseViewModel
         _navigationService = navigationService;
         _spacexService = spaceXService;
 
-        GoToDetailCommand = new AsyncRelayCommand<Launch>((param) => GoToDetail(param));
-
         LaunchesResponse = new ObservableCollection<GetLaunchesResponse>();
     }
 
     [ObservableProperty]
     private ObservableCollection<GetLaunchesResponse> _launchesResponse;
-
-    public ICommand GoToDetailCommand { private set; get; }
 
     public override async Task Initialize()
     {
@@ -42,6 +37,7 @@ public partial class LaunchesViewModel : BaseViewModel
         _coreService.HideLoading();
     }
 
+    [RelayCommand]
     private async Task GoToDetail(Launch launch)
     {
         await _navigationService.NavigateToAsync(nameof(LaunchDetailView), launch);
